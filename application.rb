a16277e0356a8fd93658e34389e1a175d664e3b7
gr1d99 <gideon.kimutai.kim@gmail.com>
2020-04-09 10:49:34 +0300

    feat: setup shrine

--- application.rb
+++ application.rb
@@ -3,6 +3,19 @@
 require 'sinatra/base'
 require 'sinatra/json'
 
+require "shrine"
+require "shrine/storage/file_system"
+
+Shrine.storages = {
+  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),
+}
+
+Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data
+Shrine.plugin :restore_cached_data
+Shrine.plugin :rack_file
+
 module Application
   class Api < Sinatra::Base
   end
